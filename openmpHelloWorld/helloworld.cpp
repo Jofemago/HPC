ef4cc3bb05445f4301f93c83148483f8e543cebf
jofemago <jofemago@utp.edu.co>
2019-05-15 15:39:14 -0500

    hello  world omp

---
+++ openmpHelloWorld/helloworld.cpp
@@ -0,0 +1,22 @@
+#include<omp.h>
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int p, th_id;
+    p= omp_get_num_procs();
+    cout << "procesos: "<< p << endl;
+    omp_set_num_threads(p);
+    #pragma omp parallel private(th_id)
+    {
+        th_id = omp_get_thread_num();
+        
+        cout << "se esta ejecutando el hilo: "+ to_string(th_id) + '\n';
+    }
+
+    return 0;
+}
