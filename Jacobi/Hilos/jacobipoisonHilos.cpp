67d152ecbee9c32ceace11b74528744afa16d8a5
jofemago <jofemago@utp.edu.co>
2019-05-14 20:28:46 -0500

    paralelizacion por hilos

--- Jacobi/Hilos/jacobipoisonHilos.cpp
+++ Jacobi/Hilos/jacobipoisonHilos.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <string>
-
+#include <thread>
 #include <chrono>
-#include<iostream>
-//#include<bits/stdc++.h>
+#include<iostream>*/
+#include<bits/stdc++.h>
 
 
 
@@ -31,28 +31,51 @@ void imprimir(string a, double *u, int n){
         
         cout<<u[i]<<" ";
     }
+    cout << endl;
 }
 
 
-void iteratetemp(int n, int h2,  double *u, double *utmp, double *f){
+void runbychunk(int i, int j, int n, int h2,  double *u, double *utmp, double *f){
 
     /* Old data in u; new data in utmp */
-        for (int i = 1; i < n; ++i){
-            utmp[i] = (u[i-1] + u[i+1] + h2*f[i])/2;
+
+    if(n+1 % 2 == 0){ //es par
+        if(i == 0){
+                //primera parte del arreglo
+                for(int h = 1; h < (n+1)/2; ++h)
+                    utmp[h] = (u[h-1] + u[h+1] + h2*f[h])/2;
+        }else{
+            //segunda parte del arreglo
+                for(int h = (n+1)/2; h < n; ++h)
+                    utmp[h] = (u[h-1] + u[h+1] + h2*f[h])/2;
         }
-        //imprimir("iteracion funcion: ", utmp, n);
+    }else{//es impar
+        if(i == 0){
+            //primera parte del arreglo
+                for(int h = 1; h <= j; ++h)
+                    utmp[h] = (u[h-1] + u[h+1] + h2*f[h])/2;
+
+        }else{
+            //segunda parte del arreglo
+            for(int h = ((n+1)/2)+1; h < n; ++h)
+                utmp[h] = (u[h-1] + u[h+1] + h2*f[h])/2;
+        }   
+
+    }
+    
+      
 }
 
-
+/*
 void iterateU(int n, int h2,  double *u, double *utmp,double *f){
 
-    /* Old data in utmp; new data in u */
+    // Old data in utmp; new data in u 
         for (int i = 1; i < n; ++i){
             u[i] = (utmp[i-1] + utmp[i+1] + h2*f[i])/2;
         }
         //imprimir("iteracion funcion: ", utmp, n);
 }
-
+*/
 
 void jacobi(int nsweeps, int n, double* u, double* f){
     double h  = 1.0 / n;
@@ -64,14 +87,32 @@ void jacobi(int nsweeps, int n, double* u, double* f){
     utmp[0] = u[0];
     utmp[n] = u[n];
 
-    cout << "h2 "<< h2 << endl;
+    /*cout << "h2 "<< h2 << endl;
     imprimir("inicio f: ", f, n);
     imprimir("inicio: ", utmp, n);
-    imprimir("inicio: ", u, n);
+    imprimir("inicio: ", u, n);*/
     for (int sweep = 0; sweep < nsweeps; sweep += 2) {
-        
-        iteratetemp(n,h2,u,utmp,f);
-        iterateU(n,h2,u,utmp,f);
+        vector<thread> th1;
+        th1.push_back(thread(runbychunk,0,(n+1)/2, n,h2,u,utmp,f));
+        th1.push_back(thread(runbychunk,(n+1)/2,n,n,h2,u,utmp,f));
+        //primer chunk
+        //runbychunk(0,(n+1)/2, n,h2,u,utmp,f);
+        //runbychunk((n+1)/2,n,n,h2,u,utmp,f);
+        for(int i = 0; i < th1.size(); i++)
+            th1[i].join();
+        vector<thread> th2;
+        //segundo chunk
+
+        th2.push_back(thread(runbychunk,0,(n+1)/2,n,h2,utmp,u,f));
+        th2.push_back(thread(runbychunk,(n+1)/2 +1, n+1, n,h2,utmp,u,f));
+        //primer chunk
+        //runbychunk(0,(n+1)/2, n,h2,u,utmp,f);
+        //runbychunk((n+1)/2,n,n,h2,u,utmp,f);
+        for(int i = 0; i < th2.size(); i++)
+            th2[i].join();
+       //runbychunk(0,(n+1)/2,n,h2,utmp,u,f);
+        //runbychunk((n+1)/2 +1, n+1, n,h2,utmp,u,f);
+
         
 
 
@@ -89,8 +130,8 @@ void jacobi(int nsweeps, int n, double* u, double* f){
 
     }
 
-    imprimir("fin: ", utmp, n);
-    imprimir("fin: ", u, n);
+    //imprimir("fin: ", utmp, n);
+    // imprimir("fin: ", u, n);
 
     delete [] utmp;
 }
