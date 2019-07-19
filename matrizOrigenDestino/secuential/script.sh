8781ce9a4e61efae47ecf0a16c50254ac36c3611
jofemago <jofemago@utp.edu.co>
2019-07-19 01:17:02 -0500

    actualiza

--- matrizOrigenDestino/secuential/script.sh
+++ matrizOrigenDestino/secuential/script.sh
@@ -9,7 +9,7 @@ for q in 1 2 3 4 5 6 7 8 9 10
 do
 	for i in 1000 10000 100000 #1000000 10000000 100000000 10000000
 	do
-      ./o.o $i 100   #>> ./data/prueba$j.csv 
+      ./o.o $i #100 10000 1000000 100000000   #>> ./data/prueba$j.csv 
 	done
 	echo $k #>> prueba$aux.csv
 
